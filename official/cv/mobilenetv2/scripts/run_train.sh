1844d42aa0cc75010a70a7b68ee74244ede20e71
zhaoting <zhaoting23@huawei.com>
2021-09-16 11:39:44 +0800

    fix num_parallel_workers greater than CPU cores

--- official/cv/mobilenetv2/scripts/run_train.sh
+++ official/cv/mobilenetv2/scripts/run_train.sh
@@ -46,10 +46,6 @@ run_ascend()
         echo "error: DATASET_PATH=$6 is not a directory or file"
     exit 1
     fi
-    RUN_DISTRIBUTE=True
-    if [ $2 -eq 1 ] ; then
-        RUN_DISTRIBUTE=False
-    fi
     BASEPATH=$(cd "`dirname $0`" || exit; pwd)
     CONFIG_FILE="${BASEPATH}/../$2"
 
@@ -69,6 +65,22 @@ run_ascend()
     fi
     mkdir ../train
     cd ../train || exit
+    RUN_DISTRIBUTE=True
+    if [ $3 -eq 1 ] ; then
+        RUN_DISTRIBUTE=False
+        export DEVICE_ID=${CANDIDATE_DEVICE[0]}
+        export RANK_ID=0
+        nohup python ${BASEPATH}/../train.py \
+            --run_distribute=$RUN_DISTRIBUTE \
+            --config_path=$CONFIG_FILE \
+            --platform=$1 \
+            --dataset_path=$6 \
+            --pretrain_ckpt=$PRETRAINED_CKPT \
+            --freeze_layer=$FREEZE_LAYER \
+            --filter_head=$FILTER_HEAD \
+            &> ../train.log &
+        exit 1
+    fi
     cpus=`cat /proc/cpuinfo| grep "processor"| wc -l`
     avg=`expr $cpus \/ $RANK_SIZE`
     gap=`expr $avg \- 1`
@@ -120,14 +132,6 @@ run_gpu()
               GPU: sh run_train.sh GPU [DEVICE_NUM] [VISIABLE_DEVICES(0,1,2,3,4,5,6,7)] [DATASET_PATH]"
         exit 1
     fi;
-    if [ $2 -eq 1 ] ; then
-        RUN_DISTRIBUTE=False
-    elif [ $2 -gt 1 ] && [ $2 -le 8 ] ; then
-        RUN_DISTRIBUTE=True
-    else
-        echo "error: DEVICE_NUM=$2 is not in (1-8)"
-        exit 1
-    fi;
 
     if [ ! -d $4 ]
     then
@@ -147,6 +151,26 @@ run_gpu()
     cd ../train || exit
 
     export CUDA_VISIBLE_DEVICES="$3"
+
+    if [ $2 -eq 1 ] ; then
+        RUN_DISTRIBUTE=False
+        nohup python ${BASEPATH}/../train.py \
+        --config_path=$CONFIG_FILE \
+        --platform=$1 \
+        --run_distribute=$RUN_DISTRIBUTE \
+        --dataset_path=$4 \
+        --pretrain_ckpt=$PRETRAINED_CKPT \
+        --freeze_layer=$FREEZE_LAYER \
+        --filter_head=$FILTER_HEAD \
+        &> ../train.log &
+        exit 1
+    elif [ $2 -gt 1 ] && [ $2 -le 8 ] ; then
+        RUN_DISTRIBUTE=True
+    else
+        echo "error: DEVICE_NUM=$2 is not in (1-8)"
+        exit 1
+    fi;
+
     mpirun -n $2 --allow-run-as-root --output-filename log_output --merge-stderr-to-stdout \
     python ${BASEPATH}/../train.py \
         --config_path=$CONFIG_FILE \
