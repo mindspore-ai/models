bf1c7e74f0f08553c9a66cd92f8fe2470ef11c33
张杰 <508610057@qq.com>
2021-09-17 09:49:55 +0800

    add all
    update official/cv/alexnet/infer/mxbase/src/Alexnet.cpp.
    
    update official/cv/alexnet/infer/data/images/cifar10.py.
    
    update official/cv/alexnet/infer/data/images/cifar10.py.
    
    update official/cv/alexnet/infer/data/images/cifar10.py.
    
    update official/cv/alexnet/default_config.yaml.
    
    update official/cv/alexnet/infer/mxbase/src/Alexnet.cpp.
    
    update official/cv/alexnet/infer/mxbase/src/Alexnet.cpp.
    
    update official/cv/alexnet/infer/mxbase/src/Alexnet.cpp.
    
    update official/cv/alexnet/infer/mxbase/src/Alexnet.cpp.
    
    update official/cv/alexnet/infer/mxbase/src/Alexnet.cpp.
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all
    
    add all

---
+++ official/cv/alexnet/infer/mxbase/src/main.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2021. Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dirent.h>
+#include "Alexnet.h"
+#include "MxBase/Log/Log.h"
+
+
+namespace {
+const uint32_t CLASS_NUM = 10;
+}
+
+APP_ERROR ScanImages(const std::string &path, std::vector<std::string> &imgFiles) {
+    DIR *dirPtr = opendir(path.c_str());
+    if (dirPtr == nullptr) {
+        LogError << "opendir failed. dir:" << path << path.c_str();
+        return APP_ERR_INTERNAL_ERROR;
+    }
+    dirent *direntPtr = nullptr;
+    while ((direntPtr = readdir(dirPtr)) != nullptr) {
+        std::string fileName = direntPtr->d_name;
+        if (fileName == "." || fileName == "..") {
+            continue;
+        }
+
+        imgFiles.emplace_back(path + "/" + fileName);
+    }
+    LogInfo << "opendir ok. dir:";
+    closedir(dirPtr);
+    return APP_ERR_OK;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        LogWarn << "Please input image path, such as './alexnet image_dir'";
+        return APP_ERR_OK;
+    }
+
+    InitParam initParam = {};
+    initParam.deviceId = 0;
+    initParam.classNum = CLASS_NUM;
+    initParam.labelPath = "../data/config/cifar10.names";
+    initParam.topk = 5;
+    initParam.softmax = false;
+    initParam.checkTensor = true;
+    initParam.modelPath = "../data/models/alexnet_bs1.om";
+    auto alexnet = std::make_shared<Alexnet>();
+    APP_ERROR ret = alexnet->Init(initParam);
+    if (ret != APP_ERR_OK) {
+        alexnet->DeInit();
+        LogError << "AlexnetClassify init failed, ret=" << ret << ".";
+        return ret;
+    }
+
+    std::string imgPath = argv[1];
+    std::vector<std::string> imgFilePaths;
+    ret = ScanImages(imgPath, imgFilePaths);
+    if (ret != APP_ERR_OK) {
+        return ret;
+    }
+    auto startTime = std::chrono::high_resolution_clock::now();
+    for (auto &imgFile : imgFilePaths) {
+        ret = alexnet->Process(imgFile);
+        if (ret !=APP_ERR_OK) {
+            LogError << "AlexnetClassify process failed, ret=" << ret << ".";
+            alexnet->DeInit();
+            return ret;
+        }
+    }
+    auto endTime = std::chrono::high_resolution_clock::now();
+    alexnet->DeInit();
+    double costMilliSecs = std::chrono::duration<double, std::milli>(endTime - startTime).count();
+    double fps = 1000.0*imgFilePaths.size() / alexnet->GetInferCostMilliSec();
+    LogInfo << "[Process Delay] cost:" << costMilliSecs << " ms\tfps: " << fps << "imgs/sec";
+    return APP_ERR_OK;
+}
+
