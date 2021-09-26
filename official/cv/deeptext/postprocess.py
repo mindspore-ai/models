46030a09da846a7f9a95fbeee50d0fe54cd44ead
zhouneng <zhouneng2@huawei.com>
2021-09-26 14:43:41 +0800

    fix models dataset preprocess bug

--- official/cv/deeptext/postprocess.py
+++ official/cv/deeptext/postprocess.py
@@ -37,9 +37,9 @@ def get_pred(file, result_path):
 
 def get_gt_bboxes_labels(label_file, img_file):
     img_data = np.array(Image.open(img_file))
+    h, w = img_data.shape[:2]
     img_data = cv2.resize(
         img_data, (config.img_width, config.img_height), interpolation=cv2.INTER_LINEAR)
-    h, w = img_data.shape[:2]
     h_scale = config.img_height / h
     w_scale = config.img_width / w
 
