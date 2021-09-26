678f9504c7c3e3c2065bbdddd56a94b34fc5e89f
yuzhenhua <yuzhenhua.yuzhenhua@huawei.com>
2021-09-26 11:19:15 +0800

    fix pip version and opp path issue

--- research/cv/ghostnet/ascend310_infer/src/build.sh
+++ research/cv/ghostnet/ascend310_infer/src/build.sh
@@ -15,5 +15,5 @@
 # ============================================================================
 
 # shellcheck disable=SC2006
-cmake . -DMINDSPORE_PATH="`pip3.7 show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+cmake . -DMINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
 make
