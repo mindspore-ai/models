acb89b585f84f5d5f8f523f511ff4932f68bc804
dinglinhe <dinglinhe@huawei.com>
2021-10-21 15:44:02 +0800

    Update mindspore_hub_conf in vgg16 at branch master

--- official/cv/vgg16/mindspore_hub_conf.py
+++ official/cv/vgg16/mindspore_hub_conf.py
@@ -14,7 +14,7 @@
 # ============================================================================
 """hub config."""
 from src.vgg import vgg16 as VGG16
-from model_utils.moxing_adapter import config
+from model_utils.config import get_config_static
 
 def vgg16(*args, **kwargs):
     return VGG16(*args, **kwargs)
@@ -22,5 +22,12 @@ def vgg16(*args, **kwargs):
 
 def create_network(name, *args, **kwargs):
     if name == "vgg16":
-        return vgg16(args=config, *args, **kwargs)
+        num_classes = kwargs.get("num_classes", 10)
+        if "num_classes" in kwargs:
+            del kwargs["num_classes"]
+        if num_classes == 10:
+            config = get_config_static(config_path="../cifar10_config.yaml")
+        elif num_classes == 1000:
+            config = get_config_static(config_path="../imagenet2012_config.yaml")
+        return vgg16(num_classes=num_classes, args=config, *args, **kwargs)
     raise NotImplementedError(f"{name} is not implemented in the repo")
