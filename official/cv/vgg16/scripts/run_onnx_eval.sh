3948ee7d2d15828fd4d95bdec035e137daff0641
Andrey Denisov <a.denisov@expasoft.tech>
2021-10-29 17:24:29 +0700

    ONNX VGG16 - export support

--- official/cv/vgg16/scripts/run_onnx_eval.sh
+++ official/cv/vgg16/scripts/run_onnx_eval.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2020 Huawei Technologies Co., Ltd
+# Copyright 2022 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,14 +16,14 @@
 
 echo "=============================================================================================================="
 echo "Please run the script as: "
-echo "bash scripts/run_eval.sh DATA_PATH DATASET_TYPE DEVICE_TYPE CHECKPOINT_PATH"
-echo "for example: bash scripts/run_eval.sh /path/ImageNet2012/validation imagenet2012 Ascend /path/a.ckpt "
+echo "bash scripts/run_onnx_eval.sh DATA_PATH DATASET_TYPE DEVICE_TYPE ONNX_MODEL_PATH"
+echo "for example: bash scripts/run_onnx_eval.sh /path/ImageNet2012/validation imagenet2012 GPU /path/a.onnx "
 echo "=============================================================================================================="
 
 DATA_PATH=$1
 DATASET_TYPE=$2
 DEVICE_TYPE=$3
-CHECKPOINT_PATH=$4
+ONNX_MODEL_PATH=$4
 
 get_real_path(){
     if [ "${1:0:1}" == "/" ]; then
@@ -36,9 +36,9 @@ get_real_path(){
 config_path=$(get_real_path "./${DATASET_TYPE}_config.yaml")
 echo "config path is : ${config_path}"
 
-python eval.py \
+python eval_onnx.py \
     --config_path=$config_path \
     --data_dir=$DATA_PATH \
     --dataset=$DATASET_TYPE \
     --device_target=$DEVICE_TYPE \
-    --pre_trained=$CHECKPOINT_PATH > output.eval.log 2>&1 &
+    --file_name=$ONNX_MODEL_PATH > output.eval_onnx.log 2>&1 &
