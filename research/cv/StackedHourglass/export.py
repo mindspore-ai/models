0026eedcf567eca32ce626599207578fa78784e7
kzx2020 <kangzhaoxiang2018@163.com>
2021-11-22 14:27:24 +0800

    Add StackHourglass ascend 310 infer code

--- research/cv/StackedHourglass/export.py
+++ research/cv/StackedHourglass/export.py
@@ -12,18 +12,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""
-export model
-"""
+"""export model"""
 import os
-
 import numpy as np
-from mindspore import Tensor, context, export, load_checkpoint, load_param_into_net
-
 from src.config import parse_args
 from src.models.StackedHourglassNet import StackedHourglassNet
+import src.dataset.MPIIDataLoader as ds
+import mindspore.nn as nn
+import mindspore.ops as ops
+from mindspore import Tensor, context, export, load_checkpoint, load_param_into_net
 
 args = parse_args()
+
+
+class MaxPool2dFilter(nn.Cell):
+    """
+    maxpool 2d for filter
+    """
+
+    def __init__(self):
+        super(MaxPool2dFilter, self).__init__()
+        self.pool = nn.MaxPool2d(3, 1, "same")
+        self.eq = ops.Equal()
+
+    def construct(self, x):
+        """
+        forward
+        """
+        maxm = self.pool(x)
+        return self.eq(maxm, x)
+
+
+class Hourglass(nn.Cell):
+    """
+        Hourglass
+    """
+
+    def __init__(self, network):
+        super(Hourglass, self).__init__(auto_prefix=False)
+        self.net = network
+        self.pool = nn.MaxPool2d(3, 1, "same")
+        self.eq = ops.Equal()
+
+    def construct(self, input1, input2):
+        """
+        forward
+        """
+        tmp1 = self.net(input1)
+        tmp2 = self.net(input2)
+        tmp = ops.Concat(0)((tmp1, tmp2))
+
+        det = tmp[0, -1] + tmp[1, -1, :, :, ::-1][ds.flipped_parts["mpii"]]
+
+        det = det / 2
+
+        det = ops.minimum(det, 1)
+        det0 = det
+        det = ops.expand_dims(det, 0)
+        maxm = self.pool(det)
+        maxm = self.eq(maxm, det)
+        det1 = det * maxm
+        return det0, det1
+
 
 if __name__ == "__main__":
     if not os.path.exists(args.ckpt_file):
@@ -40,6 +90,7 @@ if __name__ == "__main__":
     net = StackedHourglassNet(args.nstack, args.inp_dim, args.oup_dim)
     param_dict = load_checkpoint(args.ckpt_file)
     load_param_into_net(net, param_dict)
-
-    input_arr = Tensor(np.zeros([args.batch_size, args.input_res, args.input_res, 3], np.float32))
-    export(net, input_arr, file_name=args.file_name, file_format=args.file_format)
+    input_arr = Tensor(np.zeros([1, args.input_res, args.input_res, 3], np.float32))
+    input_arr2 = Tensor(np.zeros([1, args.input_res, args.input_res, 3], np.float32))
+    net = Hourglass(net)
+    export(net, input_arr, input_arr2, file_name='Hourglass', file_format=args.file_format)
