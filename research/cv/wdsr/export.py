d18428253f194f09e3a81f460369e5497121f15c
zhanghuiyao <1814619459@qq.com>
2022-01-25 10:26:16 +0800

    fix wdsr export bug

--- research/cv/wdsr/export.py
+++ research/cv/wdsr/export.py
@@ -24,13 +24,16 @@ parser.add_argument("--batch_size", type=int, default=1, help="batch size")
 parser.add_argument("--ckpt_path", type=str, required=True, help="path of checkpoint file")
 parser.add_argument("--file_name", type=str, default="wdsr", help="output file name.")
 parser.add_argument("--file_format", type=str, default="MINDIR", choices=['MINDIR', 'AIR', 'ONNX'], help="file format")
+parser.add_argument('--n_resblocks', type=int, default=16, help='number of residual blocks')
+parser.add_argument('--scale', type=int, default=2, help='super resolution scale')
+parser.add_argument('--n_feats', type=int, default=64, help='number of feature maps')
 args = parser.parse_args()
 
 def run_export(args1):
     """run_export"""
     device_id = int(os.getenv("DEVICE_ID", '0'))
     context.set_context(mode=context.GRAPH_MODE, device_target="Ascend", device_id=device_id)
-    net = wdsr.WDSR()
+    net = wdsr.WDSR(scale=args.scale, n_resblocks=args.n_resblocks, n_feats=args.n_feats)
     param_dict = load_checkpoint(args1.ckpt_path)
     load_param_into_net(net, param_dict)
     net.set_train(False)
