920639d8978a5722986b457c36fd8583b4626ce0
jiangzhenguang <jiangzhenguang1@huawei.com>
2022-02-12 15:07:38 +0800

    ssim-ae function

--- official/cv/ssim-ae/scripts/run_eval.sh
+++ official/cv/ssim-ae/scripts/run_eval.sh
@@ -29,7 +29,12 @@ get_real_path(){
 
 CONFIG_PATH=$(get_real_path $1)
 export DEVICE_ID=$2
-BASE_PATH=$(cd "`dirname $0`" || exit; pwd)
-cd $BASE_PATH/../ || exit
-python eval.py --config_path=$CONFIG_PATH > eval.log 2>&1 &
+EVAL_PATH=eval_`echo $CONFIG_PATH | rev | cut -d '/' -f 1 | rev | awk -F "_config.yaml" '{print $1}'`
+if [ -d $EVAL_PATH ];
+then
+    rm -rf $EVAL_PATH
+fi
+mkdir $EVAL_PATH
+cd $EVAL_PATH/ || exit
+python ../eval.py --config_path=$CONFIG_PATH > eval.log 2>&1 &
 tail -f eval.log
--- official/cv/ssim-ae/train.py
+++ official/cv/ssim-ae/train.py
@@ -21,6 +21,7 @@ from mindspore.context import ParallelMode
 from mindspore.communication.management import init, get_rank
 from mindspore import Model, DynamicLossScaleManager, context
 from mindspore.train.callback import ModelCheckpoint, CheckpointConfig, LossMonitor, TimeMonitor
+from mindspore.train.serialization import load_checkpoint, load_param_into_net
 
 from src.network import AutoEncoder, SSIMLoss, NetWithLoss
 from src.dataset import Dataloader
@@ -28,6 +29,7 @@ from model_utils.config import config as cfg
 from model_utils.device_adapter import get_device_id, get_device_num
 
 set_seed(1234)
+
 
 def train():
     dataloader = Dataloader()
@@ -43,15 +45,22 @@ def train():
         cfg.batch_size = cfg.batch_size // device_num
 
     if rank == 0:
-        if not os.path.exists(cfg.aug_dir):
-            os.makedirs(cfg.aug_dir)
-        if not os.path.exists(cfg.tmp):
-            os.makedirs(cfg.tmp)
+        if os.path.exists(cfg.aug_dir):
+            remove_dir(cfg.aug_dir)
+        os.makedirs(cfg.aug_dir)
+        if os.path.exists(cfg.tmp):
+            remove_dir(cfg.tmp)
+        os.makedirs(cfg.tmp)
     train_dataset = dataloader.create_dataset(device_num, rank)
     cfg.dataset_size = train_dataset.get_dataset_size()
     loss = SSIMLoss()
     auto_encoder = AutoEncoder(cfg)
     net_loss = NetWithLoss(auto_encoder, loss)
+    if cfg.load_ckpt_path != "":
+        param_dict = load_checkpoint(cfg.load_ckpt_path)
+        load_param_into_net(net_loss, param_dict)
+        print("Load checkpoint {} to net successfully".format(cfg.load_ckpt_path))
+
     cb = [LossMonitor(1), TimeMonitor()]
     optimizer = nn.AdamWeightDecay(params=auto_encoder.trainable_params(), learning_rate=cfg.lr,
                                    weight_decay=cfg.decay)
@@ -68,7 +77,7 @@ def train():
         eval_cb = EvalCallBack(cfg, auto_encoder, get_results, apply_eval)
         cb += [eval_cb]
 
-    model = Model(net_loss, optimizer=optimizer, loss_scale_manager=loss_scale_manager)
+    model = Model(net_loss, optimizer=optimizer, loss_scale_manager=loss_scale_manager, amp_level="O0")
     if rank == 0:
         cb.append(ckpoint_cb)
     model.train(cfg.epochs, train_dataset, callbacks=cb, dataset_sink_mode=True)
@@ -76,17 +85,14 @@ def train():
     if cfg.model_arts and rank == 0:
         import moxing as mox
         mox.file.copy_parallel(src_url="./checkpoint", dst_url=cfg.train_url)
-    if rank == 0:
-        if os.path.exists(cfg.aug_dir):
-            remove_dir(cfg.aug_dir)
-        if os.path.exists(cfg.tmp):
-            remove_dir(cfg.tmp)
+
 
 def remove_dir(path):
     if os.path.isfile(path):
         os.remove(path)
     elif os.path.isdir(path):
         shutil.rmtree(path, True)
+
 
 if __name__ == '__main__':
     train()
