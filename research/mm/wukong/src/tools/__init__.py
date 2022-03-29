d6926d8c3caa46fdbe77241f390b233bbc712320
nmzfrank <nmzfrank@sjtu.edu.cn>
2022-03-29 12:05:38 +0800

    update filip model with data support

--- research/mm/wukong/src/tools/__init__.py
+++ research/mm/wukong/src/tools/__init__.py
@@ -13,7 +13,9 @@
 # limitations under the License.
 # ============================================================================
 from .template_generate import generate_zh_template
-from .model_utils import load_model, load_visual_model, load_text_model
+from .model_utils import load_visual_model, load_text_model
+from .simple_tokenizer import set_tokenizer_lang, tokenize
 
 
-__all__ = ['generate_zh_template', 'load_model', 'load_text_model', 'load_visual_model']
+__all__ = ['generate_zh_template', 'load_visual_model',
+           'load_text_model', 'set_tokenizer_lang', 'tokenize']
