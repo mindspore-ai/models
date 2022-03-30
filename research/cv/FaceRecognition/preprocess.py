6cda6fd7d19e1621648d8f012e93b4889bc7b19a
zyhStack <zhangyihui7@huawei.com>
2022-03-30 15:02:17 +0800

    modify the sensitive word of Face Recognition

--- research/cv/FaceRecognition/preprocess.py
+++ research/cv/FaceRecognition/preprocess.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Huawei Technologies Co., Ltd
+# Copyright 2021-2022 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Face Recognition infer preprocess."""
+"""Object Recognition infer preprocess."""
 import os
 import shutil
 
--- research/cv/FaceRecognitionForTracking/src/loss.py
+++ research/cv/FaceRecognitionForTracking/src/loss.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Huawei Technologies Co., Ltd
+# Copyright 2020-2022 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Face Recognition loss."""
+"""Object Recognition loss."""
 from mindspore.nn.loss.loss import LossBase
 from mindspore.ops import operations as P
 from mindspore.ops import functional as F
