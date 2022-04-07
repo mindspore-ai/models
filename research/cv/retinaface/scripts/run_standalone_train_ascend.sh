52185fac6fc6375d0aa8699dab655f28b3c04810
zhanghuiyao <1814619459@qq.com>
2022-04-07 15:18:17 +0800

    fix research/cv/retinaface bug

--- research/cv/retinaface/scripts/run_standalone_train_ascend.sh
+++ research/cv/retinaface/scripts/run_standalone_train_ascend.sh
@@ -16,4 +16,9 @@
 
 echo "Usage: bash ./scripts/run_standalone_train_ascend.sh"
 
+export DEVICE_NUM=1
+export DEVICE_ID=0
+export RANK_ID=0
+export RANK_SIZE=1
+
 python train.py --backbone_name 'ResNet50' > train.log 2>&1 &
