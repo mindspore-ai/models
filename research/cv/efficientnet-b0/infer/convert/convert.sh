d4313e322820939c3d3276731eba5d233ac754f0
Madeline <cmm_314159@163.com>
2022-04-19 09:31:28 +0800

    first commit

---
+++ research/cv/efficientnet-b0/infer/convert/convert.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+model_path=$1
+output_model_name=$2
+aipp_cfg=$3
+
+atc \
+    --model=$model_path \
+    --framework=1 \
+    --output=$output_model_name \
+    --input_format=NCHW --input_shape="actual_input_1:1,3,224,224" \
+    --enable_small_channel=1 \
+    --log=error \
+    --soc_version=Ascend310 \
+    --insert_op_conf=$aipp_cfg \
+    --output_type=FP32
