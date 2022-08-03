fc55c07d18925400437202cb76215e8fe47879b9
anzhengqi <anzhengqi1@huawei.com>
2022-08-03 09:18:25 +0800

    remove part of lr functions in yolov3_darknet53 network

--- official/cv/yolov3_darknet53/src/lr_scheduler.py
+++ official/cv/yolov3_darknet53/src/lr_scheduler.py
@@ -150,68 +150,6 @@ def warmup_cosine_annealing_lr(lr, steps_per_epoch, warmup_epochs, max_epoch, T_
     return np.array(lr_each_step).astype(np.float32)
 
 
-def warmup_cosine_annealing_lr_V2(lr, steps_per_epoch, warmup_epochs, max_epoch, T_max, eta_min=0):
-    """Cosine annealing learning rate V2."""
-    base_lr = lr
-    warmup_init_lr = 0
-    total_steps = int(max_epoch * steps_per_epoch)
-    warmup_steps = int(warmup_epochs * steps_per_epoch)
-
-    last_lr = 0
-    last_epoch_V1 = 0
-
-    T_max_V2 = int(max_epoch*1/3)
-
-    lr_each_step = []
-    for i in range(total_steps):
-        last_epoch = i // steps_per_epoch
-        if i < warmup_steps:
-            lr = linear_warmup_lr(i + 1, warmup_steps, base_lr, warmup_init_lr)
-        else:
-            if i < total_steps*2/3:
-                lr = eta_min + (base_lr - eta_min) * (1. + math.cos(math.pi*last_epoch / T_max)) / 2
-                last_lr = lr
-                last_epoch_V1 = last_epoch
-            else:
-                base_lr = last_lr
-                last_epoch = last_epoch-last_epoch_V1
-                lr = eta_min + (base_lr - eta_min) * (1. + math.cos(math.pi * last_epoch / T_max_V2)) / 2
-
-        lr_each_step.append(lr)
-    return np.array(lr_each_step).astype(np.float32)
-
-
-def warmup_cosine_annealing_lr_sample(lr, steps_per_epoch, warmup_epochs, max_epoch, T_max, eta_min=0):
-    """Warmup cosine annealing learning rate."""
-    start_sample_epoch = 60
-    step_sample = 2
-    tobe_sampled_epoch = 60
-    end_sampled_epoch = start_sample_epoch + step_sample*tobe_sampled_epoch
-    max_sampled_epoch = max_epoch+tobe_sampled_epoch
-    T_max = max_sampled_epoch
-
-    base_lr = lr
-    warmup_init_lr = 0
-    total_steps = int(max_epoch * steps_per_epoch)
-    total_sampled_steps = int(max_sampled_epoch * steps_per_epoch)
-    warmup_steps = int(warmup_epochs * steps_per_epoch)
-
-    lr_each_step = []
-
-    for i in range(total_sampled_steps):
-        last_epoch = i // steps_per_epoch
-        if last_epoch in range(start_sample_epoch, end_sampled_epoch, step_sample):
-            continue
-        if i < warmup_steps:
-            lr = linear_warmup_lr(i + 1, warmup_steps, base_lr, warmup_init_lr)
-        else:
-            lr = eta_min + (base_lr - eta_min) * (1. + math.cos(math.pi*last_epoch / T_max)) / 2
-        lr_each_step.append(lr)
-
-    assert total_steps == len(lr_each_step)
-    return np.array(lr_each_step).astype(np.float32)
-
-
 def get_lr(args):
     """generate learning rate."""
     if args.lr_scheduler == 'exponential':
@@ -229,20 +167,7 @@ def get_lr(args):
                                         args.max_epoch,
                                         args.T_max,
                                         args.eta_min)
-    elif args.lr_scheduler == 'cosine_annealing_V2':
-        lr = warmup_cosine_annealing_lr_V2(args.lr,
-                                           args.steps_per_epoch,
-                                           args.warmup_epochs,
-                                           args.max_epoch,
-                                           args.T_max,
-                                           args.eta_min)
-    elif args.lr_scheduler == 'cosine_annealing_sample':
-        lr = warmup_cosine_annealing_lr_sample(args.lr,
-                                               args.steps_per_epoch,
-                                               args.warmup_epochs,
-                                               args.max_epoch,
-                                               args.T_max,
-                                               args.eta_min)
     else:
-        raise NotImplementedError(args.lr_scheduler)
+        raise NotImplementedError("args.lr_scheduler only support 'exponential' and 'cosine_annealing'"
+                                  ", {} is not supported".format(args.lr_scheduler))
     return lr
