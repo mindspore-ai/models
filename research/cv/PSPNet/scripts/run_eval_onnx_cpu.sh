c0537b98751cf075127a7c14dbb8a099d9428e22
格物致知 <532925744@qq.com>
2022-08-08 13:55:35 +0800

    [武汉理工大学][ONNX]PSPNet

--- research/cv/PSPNet/scripts/run_eval_onnx_cpu.sh
+++ research/cv/PSPNet/scripts/run_eval_onnx_cpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 Huawei Technologies Co., Ltd
+# Copyright 2022 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,13 +14,11 @@
 # limitations under the License.
 # ============================================================================
 
-if [ $# != 2 ]
+if [ $# != 1 ]
 then
     echo "=============================================================================================================="
-    echo "Usage: bash /PSPNet/scripts/run_eval.sh [YAML_PATH] [DEVICE_ID]"
-    echo "Warning: before cpu infer, you need check device_target in config file."
-    echo "for gpu example: bash PSPNet/scripts/run_eval.sh PSPNet/config/voc2012_pspnet50.yaml 0"
-    echo "for cpu example: bash PSPNet/scripts/run_eval.sh PSPNet/config/voc2012_pspnet50.yaml cpu"
+    echo "Usage: bash /PSPNet/scripts/run_eval_onnx_cpu.sh [YAML_PATH]"
+    echo "for example: bash PSPNet/scripts/run_eval_onnx_cpu.sh PSPNet/config/voc2012_pspnet50.yaml"
     echo "=============================================================================================================="
     exit 1
 fi
@@ -30,14 +28,7 @@ mkdir ./LOG
 export YAML_PATH=$1
 export RANK_SIZE=1
 export RANK_ID=0
-export DEVICE_ID=$2
-echo "start evaluating for device $DEVICE_ID"
+echo "start evaluating on CPU"
 env > env.log
-if [ "$2" == "cpu" ]
-then
-    python3 eval_cpu.py --config="$YAML_PATH" > ./LOG/eval_log.txt 2>&1 &
-fi
-if [ "$2" != "cpu" ]
-then
-    python3 eval.py --config="$YAML_PATH" > ./LOG/eval_log.txt 2>&1 &
-fi
+
+python3 eval_onnx_cpu.py --config="$YAML_PATH" > ./LOG/eval_onnx.txt 2>&1 &
