6838aeed074f21598e5b56ca52c22d0415e83582
hemaohua <hemaohua@huawei.com>
2022-09-16 10:47:32 +0800

    update eval_callback for SPPNet

--- research/cv/SPPNet/src/eval_callback.py
+++ research/cv/SPPNet/src/eval_callback.py
@@ -54,10 +54,7 @@ class EvalCallBack(Callback):
         self.interval = interval
         self.save_best_ckpt = save_best_ckpt
         self.best_epoch = 0
-        if train_model_name == "sppnet_single":
-            self.best_res = {'top_1_accuracy': 0.64, 'top_5_accuracy': 0.84}
-        else:
-            self.best_res = {'top_1_accuracy': 0.63, 'top_5_accuracy': 0.84}
+        self.best_res = {'top_1_accuracy': 0.0, 'top_5_accuracy': 0.0}
         if not os.path.isdir(ckpt_directory):
             os.makedirs(ckpt_directory)
         self.bast_ckpt_path = os.path.join(ckpt_directory, besk_ckpt_name)
