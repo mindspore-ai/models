8d960fe5e00a80cfdbd3753735113a6c59a43f34
zhaoting <zhaoting23@huawei.com>
2022-10-09 19:54:01 +0800

    Revert "change gs nn metric to train metric"
    This reverts commit cac7762d673dc4852a1c2e926fb7170f26011664.

--- official/cv/lenet/eval.py
+++ official/cv/lenet/eval.py
@@ -28,7 +28,7 @@ import mindspore.nn as nn
 from mindspore import context
 from mindspore.train.serialization import load_checkpoint, load_param_into_net
 from mindspore.train import Model
-from mindspore.train.metrics import Accuracy
+from mindspore.nn.metrics import Accuracy
 
 
 def modelarts_process():
