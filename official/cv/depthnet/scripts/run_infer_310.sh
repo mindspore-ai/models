cacbb80d66a0090eb25845bc034cb6da5964795b
JoeyLin <au3c2@hotmail.com>
2022-10-10 20:45:43 +0800

    fix some path problem on branch r1.9 in official

--- official/cv/depthnet/scripts/run_infer_310.sh
+++ official/cv/depthnet/scripts/run_infer_310.sh
@@ -47,18 +47,13 @@ echo $device_id
 
 export ASCEND_HOME=/usr/local/Ascend/
 if [ -d ${ASCEND_HOME}/ascend-toolkit ]; then
-    export PATH=$ASCEND_HOME/fwkacllib/bin:$ASCEND_HOME/fwkacllib/ccec_compiler/bin:$ASCEND_HOME/ascend-toolkit/latest/fwkacllib/ccec_compiler/bin:$ASCEND_HOME/ascend-toolkit/latest/atc/bin:$PATH
-    export LD_LIBRARY_PATH=$ASCEND_HOME/fwkacllib/lib64:/usr/local/lib:$ASCEND_HOME/ascend-toolkit/latest/atc/lib64:$ASCEND_HOME/ascend-toolkit/latest/fwkacllib/lib64:$ASCEND_HOME/driver/lib64:$ASCEND_HOME/add-ons:$LD_LIBRARY_PATH
-    export TBE_IMPL_PATH=$ASCEND_HOME/ascend-toolkit/latest/opp/op_impl/built-in/ai_core/tbe
-    export PYTHONPATH=$ASCEND_HOME/fwkacllib/python/site-packages:${TBE_IMPL_PATH}:$ASCEND_HOME/ascend-toolkit/latest/fwkacllib/python/site-packages:$PYTHONPATH
-    export ASCEND_OPP_PATH=$ASCEND_HOME/ascend-toolkit/latest/opp
+    export ASCEND_HOME=/usr/local/Ascend/ascend-toolkit/latest
 else
-    export ASCEND_HOME=/usr/local/Ascend/latest/
-    export PATH=$ASCEND_HOME/fwkacllib/bin:$ASCEND_HOME/fwkacllib/ccec_compiler/bin:$ASCEND_HOME/atc/ccec_compiler/bin:$ASCEND_HOME/atc/bin:$PATH
-    export LD_LIBRARY_PATH=$ASCEND_HOME/fwkacllib/lib64:/usr/local/lib:$ASCEND_HOME/atc/lib64:$ASCEND_HOME/acllib/lib64:$ASCEND_HOME/driver/lib64:$ASCEND_HOME/add-ons:$LD_LIBRARY_PATH
-    export PYTHONPATH=$ASCEND_HOME/fwkacllib/python/site-packages:$ASCEND_HOME/atc/python/site-packages:$PYTHONPATH
-    export ASCEND_OPP_PATH=$ASCEND_HOME/opp
+    export ASCEND_HOME=/usr/local/Ascend/latest
 fi
+export PATH=$ASCEND_HOME/compiler/ccec_compiler/bin:$PATH
+export LD_LIBRARY_PATH=$ASCEND_HOME/lib64:/usr/local/Ascend/driver/lib64:$LD_LIBRARY_PATH
+export ASCEND_OPP_PATH=$ASCEND_HOME/opp
 
 function compile_app()
 {
