3525feaea9078b5d9ce41ac7c839750e558821c6
zhoufeng <zhoufeng54@huawei.com>
2022-10-12 14:26:27 +0800

    change mindspore-ascend package name
    Signed-off-by: zhoufeng <zhoufeng54@huawei.com>

--- research/cv/AVA_hpa/ascend310_infer/build.sh
+++ research/cv/AVA_hpa/ascend310_infer/build.sh
@@ -24,6 +24,9 @@ if [ -f "Makefile" ]; then
   make clean
 fi
 
-cmake .. \
-    -DMINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+MINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+if [[ ! $MINDSPORE_PATH ]];then
+    MINDSPORE_PATH="`pip show mindspore | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+fi
+cmake .. -DMINDSPORE_PATH=$MINDSPORE_PATH
 make
