4fa7886ba0a5fb3a1f1a8c3f0298609fe5667dc2
anzhengqi <anzhengqi1@huawei.com>
2022-11-21 19:32:40 +0800

    change mindspore-ascend package name in branch r1.8

--- official/cv/octsqueeze/ascend310_infer/build.sh
+++ official/cv/octsqueeze/ascend310_infer/build.sh
@@ -18,6 +18,9 @@ if [ ! -d builds ]; then
   mkdir builds
 fi
 cd builds || exit
-cmake .. \
-    -DMINDSPORE_PATH="`pip3 show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+MINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+if [[ ! $MINDSPORE_PATH ]];then
+    MINDSPORE_PATH="`pip show mindspore | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+fi
+cmake .. -DMINDSPORE_PATH=$MINDSPORE_PATH
 make
--- research/cv/hed/infer/convert/convert_om.sh
+++ research/cv/hed/infer/convert/convert_om.sh
@@ -17,6 +17,16 @@
 model_path=$1
 output_model_name=$2
 
+if [ -f "Makefile" ]; then
+  make clean
+fi
+
+MINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+if [[ ! $MINDSPORE_PATH ]];then
+    MINDSPORE_PATH="`pip show mindspore | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+fi
+cmake .. -DMINDSPORE_PATH=$MINDSPORE_PATH
+make
 
 atc \
 --model=$model_path \
