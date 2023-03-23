b01c07c23e04c7cdae8f6d0a08575d5cee216767
anzhengqi <anzhengqi1@huawei.com>
2023-03-23 14:28:59 +0800

    change mindspore-ascend package name in branch r1.10

--- official/audio/ecapa_tdnn/ascend310_infer/build.sh
+++ official/audio/ecapa_tdnn/ascend310_infer/build.sh
@@ -18,6 +18,9 @@ if [ ! -d build ]; then
   mkdir build
 fi
 cd build || exit
-cmake .. \
-    -DMINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+MINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+if [[ ! $MINDSPORE_PATH ]];then
+    MINDSPORE_PATH="`pip show mindspore | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+fi
+cmake .. -DMINDSPORE_PATH=$MINDSPORE_PATH
 make
--- research/cv/yolox/ascend310_infer/build.sh
+++ research/cv/yolox/ascend310_infer/build.sh
@@ -17,6 +17,9 @@ if [ ! -d out ]; then
   mkdir out
 fi
 cd out || exit
-cmake .. \
-    -DMINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+MINDSPORE_PATH="`pip show mindspore-ascend | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+if [[ ! $MINDSPORE_PATH ]];then
+    MINDSPORE_PATH="`pip show mindspore | grep Location | awk '{print $2"/mindspore"}' | xargs realpath`"
+fi
+cmake .. -DMINDSPORE_PATH=$MINDSPORE_PATH
 make
