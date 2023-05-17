10c8977df7efb77271b12773fa560b039988be69
jianyunchao <jycade@163.com>
2023-05-17 19:39:46 +0800

    gpu: build network in fp32

--- research/cv/FaceRecognitionForTracking/train.py
+++ research/cv/FaceRecognitionForTracking/train.py
@@ -183,7 +183,7 @@ def run_train():
         cfg.logger.info('load model %s success', cfg.pretrained)
 
     # mixed precision training
-    if cfg.device_target == 'CPU':
+    if cfg.device_target != 'Ascend':
         network.add_flags_recursive(fp32=True)
         head.add_flags_recursive(fp32=True)
     else:
